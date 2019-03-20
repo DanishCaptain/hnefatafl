736424694e62dc5d0c7f8f5e9d5e6e0441ca7e47
Brian Sorensen <brian.sorensen@freestar.io>
2019-03-20 00:22:58 -0700

    add initial button listener

--- app/src/main/java/com/danishcaptain/games/hnefatafl/MainActivity.java
+++ app/src/main/java/com/danishcaptain/games/hnefatafl/MainActivity.java
@@ -2,15 +2,17 @@ package com.danishcaptain.games.hnefatafl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.danishcaptain.games.hnefatafl.domain.Defender;
 import com.danishcaptain.games.hnefatafl.domain.King;
 import com.danishcaptain.games.hnefatafl.domain.Offender;
 import com.danishcaptain.games.hnefatafl.widget.CenterCastle;
 import com.danishcaptain.games.hnefatafl.widget.DefenseLocation;
+import com.danishcaptain.games.hnefatafl.widget.EmptyLocation;
 import com.danishcaptain.games.hnefatafl.widget.OffenseLocation;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +21,18 @@ public class MainActivity extends AppCompatActivity {
 
         createKing(R.id.buttonCC);
 
-        createDefender(R.id.button6_4);
-        createDefender(R.id.button5_5);
-        createDefender(R.id.button6_5);
-        createDefender(R.id.button7_5);
-        createDefender(R.id.button4_6);
-        createDefender(R.id.button5_6);
-        createDefender(R.id.button7_6);
-        createDefender(R.id.button8_6);
-        createDefender(R.id.button5_7);
-        createDefender(R.id.button6_7);
-        createDefender(R.id.button7_7);
-        createDefender(R.id.button6_8);
+        createDefender(R.id.buttonD1);
+        createDefender(R.id.buttonD2);
+        createDefender(R.id.buttonD3);
+        createDefender(R.id.buttonD4);
+        createDefender(R.id.buttonD5);
+        createDefender(R.id.buttonD6);
+        createDefender(R.id.buttonD7);
+        createDefender(R.id.buttonD8);
+        createDefender(R.id.buttonD9);
+        createDefender(R.id.buttonD10);
+        createDefender(R.id.buttonD11);
+        createDefender(R.id.buttonD12);
 
         createOffender(R.id.buttonOT1);
         createOffender(R.id.buttonOT2);
@@ -67,20 +69,107 @@ public class MainActivity extends AppCompatActivity {
         createOffender(R.id.buttonOB6);
         createOffender(R.id.buttonOB7);
         createOffender(R.id.buttonOB8);
+
+        initEmpty(R.id.buttonE1);
+        initEmpty(R.id.buttonE2);
+        initEmpty(R.id.buttonE3);
+        initEmpty(R.id.buttonE4);
+        initEmpty(R.id.buttonE5);
+        initEmpty(R.id.buttonE6);
+        initEmpty(R.id.buttonE7);
+        initEmpty(R.id.buttonE8);
+        initEmpty(R.id.buttonE9);
+        initEmpty(R.id.buttonE10);
+        initEmpty(R.id.buttonE11);
+        initEmpty(R.id.buttonE12);
+        initEmpty(R.id.buttonE13);
+        initEmpty(R.id.buttonE14);
+        initEmpty(R.id.buttonE15);
+        initEmpty(R.id.buttonE16);
+        initEmpty(R.id.buttonE17);
+        initEmpty(R.id.buttonE18);
+        initEmpty(R.id.buttonE19);
+        initEmpty(R.id.buttonE20);
+        initEmpty(R.id.buttonE21);
+        initEmpty(R.id.buttonE22);
+        initEmpty(R.id.buttonE23);
+        initEmpty(R.id.buttonE24);
+        initEmpty(R.id.buttonE25);
+        initEmpty(R.id.buttonE26);
+        initEmpty(R.id.buttonE27);
+        initEmpty(R.id.buttonE28);
+        initEmpty(R.id.buttonE29);
+        initEmpty(R.id.buttonE30);
+        initEmpty(R.id.buttonE31);
+        initEmpty(R.id.buttonE32);
+        initEmpty(R.id.buttonE33);
+        initEmpty(R.id.buttonE34);
+        initEmpty(R.id.buttonE35);
+        initEmpty(R.id.buttonE36);
+        initEmpty(R.id.buttonE37);
+        initEmpty(R.id.buttonE38);
+        initEmpty(R.id.buttonE39);
+        initEmpty(R.id.buttonE40);
+        initEmpty(R.id.buttonE41);
+        initEmpty(R.id.buttonE42);
+        initEmpty(R.id.buttonE43);
+        initEmpty(R.id.buttonE44);
+        initEmpty(R.id.buttonE45);
+        initEmpty(R.id.buttonE46);
+        initEmpty(R.id.buttonE47);
+        initEmpty(R.id.buttonE48);
+        initEmpty(R.id.buttonE49);
+        initEmpty(R.id.buttonE50);
+        initEmpty(R.id.buttonE51);
+        initEmpty(R.id.buttonE52);
+        initEmpty(R.id.buttonE53);
+        initEmpty(R.id.buttonE54);
+        initEmpty(R.id.buttonE55);
+        initEmpty(R.id.buttonE56);
+        initEmpty(R.id.buttonE57);
+        initEmpty(R.id.buttonE58);
+        initEmpty(R.id.buttonE59);
+        initEmpty(R.id.buttonE60);
+        initEmpty(R.id.buttonE61);
+        initEmpty(R.id.buttonE62);
+        initEmpty(R.id.buttonE63);
+        initEmpty(R.id.buttonE64);
+        initEmpty(R.id.buttonE65);
+        initEmpty(R.id.buttonE66);
+        initEmpty(R.id.buttonE67);
+        initEmpty(R.id.buttonE68);
+        initEmpty(R.id.buttonE69);
+        initEmpty(R.id.buttonE70);
     }
 
     private void createKing(int id) {
         CenterCastle c1 = findViewById(id);
-        c1.add(new King());
+        c1.setOnClickListener(this);
+        King k = new King();
+        c1.add(k);
     }
 
     private void createDefender(int id) {
         DefenseLocation c1 = findViewById(id);
-        c1.add(new Defender());
+        c1.setOnClickListener(this);
+        Defender d = new Defender();
+        c1.add(d);
     }
 
     private void createOffender(int id) {
         OffenseLocation c1 = findViewById(id);
-        c1.add(new Offender());
+        c1.setOnClickListener(this);
+        Offender o = new Offender();
+        c1.add(o);
+    }
+
+    private void initEmpty(int id) {
+        EmptyLocation c1 = findViewById(id);
+        c1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        System.out.println("bks: "+v.getId());
     }
 }
