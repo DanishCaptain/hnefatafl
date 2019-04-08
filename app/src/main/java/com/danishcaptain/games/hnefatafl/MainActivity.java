4b23c67acd8627919b0e51223ebfad4af81558eb
Brian Sorensen <brian.sorensen@freestar.io>
2019-04-08 14:18:30 -0700

    basic

--- app/src/main/java/com/danishcaptain/games/hnefatafl/MainActivity.java
+++ app/src/main/java/com/danishcaptain/games/hnefatafl/MainActivity.java
@@ -2,16 +2,21 @@ package com.danishcaptain.games.hnefatafl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.danishcaptain.games.hnefatafl.model.GameModel;
-import com.danishcaptain.games.hnefatafl.widget.BannerAdView;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherAdView;
 
 public class MainActivity extends AppCompatActivity {
+    private PublisherAdView adView1;
+    private PublisherAdView adView2;
 
-    private BannerAdView adTextView;
+//    private BannerAdView adTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +36,23 @@ public class MainActivity extends AppCompatActivity {
         TextView infoTextView = findViewById(R.id.infoText);
         GameModel.getInstance().getPlayerInstance().initInfo(infoTextView);
 
+        adView2 = findViewById(R.id.publisherAdView);
+        PublisherAdRequest adRequest2 = new PublisherAdRequest.Builder().build();
+        adView2.loadAd(adRequest2);
 
-        adTextView = findViewById(R.id.ad);
-        adTextView.setText("Frogs");
+        LinearLayout base = findViewById(R.id.base);
+        adView1 = new PublisherAdView(this);
+        adView1.setAdSizes(AdSize.BANNER, new AdSize(300, 250));
+        adView1.setAdUnitId("/6499/example/banner");
+        PublisherAdRequest adRequest1 = new PublisherAdRequest.Builder().build();
+        adView1.loadAd(adRequest1);
+
+        ViewGroup.LayoutParams p = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        base.addView(adView1, p);
+
+
     }
 
 }
